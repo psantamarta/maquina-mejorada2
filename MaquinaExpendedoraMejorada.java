6e6eb49badf9bac6f5ce0919793881182dcd33f8
Patricia <p.santamartamedina@gmail.com>
2020-11-20 13:00:54 +0100

    0745.02 Se incluye un método que vacia la maquina del dinero tanto del cliente actual como el que lleva acumulado e indica el dinero que recauda

--- MaquinaExpendedoraMejorada.java
+++ MaquinaExpendedoraMejorada.java
@@ -86,4 +86,11 @@ public class MaquinaExpendedoraMejorada {
         balanceClienteActual = 0;
         return cantidadDeDineroADevolver;
     } 
+    
+    public int vaciarDineroDeLaMaquina (){
+        int importeTotalMaquina = balanceClienteActual + totalDineroAcumulado;
+        balanceClienteActual = 0;
+        totalDineroAcumulado = 0;
+        return importeTotalMaquina;
+    }
 }
